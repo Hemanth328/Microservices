45de9de89a14c6a197b177d15e494270095dbe8f
Hemanth328 <hemanth.blueburn11@gmail.com>
2023-12-20 11:20:20 +0530

    Dealing with config server by external server(Github/Gitlab) and native server(local)

---
+++ MSProj05-DeptService/src/test/java/com/org/MsProj05DeptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProj05DeptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
